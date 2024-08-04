8323362cf3ecacbd60f78a8f2468c024890b98ee
Yevgen <osadchyi15@gmail.com>
2024-08-04 20:34:03 +0200

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ContactForm from "./components/ContactForm/ContactForm";
 import SearchBox from "./components/SearchBox/SearchBox";
 import { nanoid } from "nanoid";
 import Contact from "./components/Contact/Contact";
+import ContactList from "./components/ContactList/ContactList";
 
 function App() {
   const [contacts, setContacts] = useState(() => {
@@ -30,18 +31,18 @@ function App() {
     setFilterValue(value);
   };
 
-  const filteredContacts = contacts.filter((contact) =>
+  const contactArray = contacts.filter((contact) =>
     contact.name.toLowerCase().includes(filterValue.toLowerCase())
   );
 
   const onDeletContact = (contactId) => {
-    setContacts(contacts.filter((contact) => contact.id !== contactId));
+    setContacts((contacts) => {
+      return contacts.filter((contact) => contact.id !== contactId);
+    });
   };
 
-  console.log("contacts", contacts);
-
   return (
-    <div>
+    <div className="wrapper">
       <h1 className="title">Phonebook</h1>
       <ContactForm onAddContact={onAddContact} />
       <SearchBox filterValue={filterValue} handleFilter={handleFilter} />
@@ -52,20 +53,25 @@ function App() {
             <p>Please add your first contact to the phonebook!</p>
           </div>
         ) : (
-          <ul>
-            {filteredContacts.map((contact) => {
-              return (
-                <li key={contact.id}>
-                  <Contact
-                    id={contact.id}
-                    name={contact.name}
-                    number={contact.number}
-                    onDeletContact={onDeletContact}
-                  />
-                </li>
-              );
-            })}
-          </ul>
+          <ContactList
+            contacts={contactArray}
+            onDeletContact={onDeletContact}
+          />
+
+          // <ul>
+          //   {filteredContacts.map((contact) => {
+          //     return (
+          //       <li key={contact.id}>
+          //         <Contact
+          //           id={contact.id}
+          //           name={contact.name}
+          //           number={contact.number}
+          //           onDeletContact={onDeletContact}
+          //         />
+          //       </li>
+          //     );
+          //   })}
+          // </ul>
         )}
       </div>
     </div>
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,13 +2,18 @@ import { useEffect, useState } from "react";
 import css from "./ContactList.module.css";
 import Contact from "../Contact/Contact";
 
-const ContactList = ({ filteredContacts }) => {
+const ContactList = ({ contacts, onDeletContact }) => {
   return (
     <ul>
-      {filteredContacts.map((contact) => {
+      {contacts.map((contact) => {
         return (
           <li key={contact.id}>
-            <Contact name={contact.name} number={contact.number} />
+            <Contact
+              name={contact.name}
+              number={contact.number}
+              onDeletContact={onDeletContact}
+              id={contact.id}
+            />
           </li>
         );
       })}
